e5d811bd51fa52885938a6e12ce07d334251b4e7
PerchunPak <perchunpak@gmail.com>
2022-10-24 16:38:24 +0200

    Setup logging

--- czech_plus/__init__.py
+++ czech_plus/__init__.py
@@ -1,1 +1,26 @@
-"""Main package for ``czech-plus``."""
+"""Main package for ``czech-plus`` addon."""
+import typing
+
+from loguru import logger
+import sys
+from czech_plus import config
+
+
+def setup_logging() -> None:
+    kwargs: dict[str, typing.Any] = {}  # type: ignore[misc] # Explicit "Any" is not allowed
+
+    if config.config.logging.level <= config.LogLevel.DEBUG:
+        kwargs["diagnose"] = True
+
+    logger.add(
+        sys.stdout,
+        level=config.config.logging.level,
+        filter="czech_plus",
+        colorize=True,
+        serialize=config.config.logging.json,
+        backtrace=True,
+        **kwargs,
+    )
+
+
+setup_logging()
